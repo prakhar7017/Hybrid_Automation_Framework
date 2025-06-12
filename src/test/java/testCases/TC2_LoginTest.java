0ec42ab64b6024f178d95d95ccea7caa7a76e9ce
Prakhar Maheshwari <prakharmaheshwari96@gmail.com>
2025-06-13 02:08:50 +0530

    completed login automation and started data driven test

---
+++ src/test/java/testCases/TC2_LoginTest.java
@@ -0,0 +1,34 @@
+package testCases;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pageObjects.HomePage;
+import pageObjects.LoginPage;
+import pageObjects.MyAccountPage;
+import testBase.BaseClass;
+
+public class TC2_LoginTest extends BaseClass {
+
+    @Test()
+    public void verify_login(){
+        LOGGER.info("****** Starting TC002_LoginTest ******");
+        try{
+            HomePage hp=new HomePage(driver,wait);
+            hp.clickMyAccount();
+            hp.clickLogin();
+
+            LoginPage lp=new LoginPage(driver,wait);
+            lp.setEmail(p.getProperty("email"));
+            lp.setPass(p.getProperty("password"));
+            lp.clickLoginBtn();
+
+            MyAccountPage macc=new MyAccountPage(driver,wait);
+            boolean targetPage=macc.isMyAccountPageExist();
+            Assert.assertTrue(targetPage);
+        }catch(Exception e){
+            LOGGER.info("error in login test->"+e.getMessage());
+            Assert.fail();
+        }
+        LOGGER.info("****** Finished TC002_LoginTest ******");
+    }
+}
